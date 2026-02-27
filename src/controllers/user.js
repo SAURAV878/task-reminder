3f4a981ef2a2cd5854fe038866916969adbbb885
Saurav <ghimiresaurav789@gmail.com>
2026-02-27 14:04:46 +0545

    feat: add jwt token for security

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,13 +1,15 @@
 import bcrypt from 'bcrypt'
 import db from '../models/index.js'
+import user from '../models/user.js';
+import jwt from 'jsonwebtoken';
 
 const User = db.User;
 
 export const register = async  (req, res) => {
+    const {firstName, lastName, email, password} = req.body;
 
     try {
-        const {firstName, lastName, email, password} = req.body;
-
+        
         const existingUser = await User.findOne ({where: {email}});
         if(existingUser) {
             return res.status (400).json({
@@ -44,3 +46,36 @@ export const register = async  (req, res) => {
     }    
 }
 
+
+export const login = async (req, res) => {
+    const {email, password} = req.body
+    try {
+
+        const existingUser = await User.findOne({where: {email}});
+        if(!existingUser) {
+            return res.status(401).json({
+                message: "Invalid credentails"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, existingUser.password);
+        if(!isMatch) {
+            return res.status(401).json({
+                message : "Invalid credentails"
+            });
+        }
+
+        const token = jwt.sign(
+            {id: existingUser.id},
+            process.env.JWT_SECRET,
+            {expiresIn : '60s'}
+        );
+
+        res.status(200).json({ token });
+
+    } catch (err) {
+        res.status(500).json ({
+            message: 'server error'
+        });
+    }
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,16 @@
 import express from "express";
 
-import { register } from "../controllers/user.js";
+import { login, register } from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get('/register', (req, res) => {
+// router.get('/register', (req, res) => {
 
-    res.send('register is working');
-})
+//     res.send('register is working');
+// })
 
 router.post('/register', register);
+router.post('/login', login);
 
 
 export default router;
